d3b75cac1fab0d1c60ad826730870d39428d2be2
Ondřej Berčík <ondrej.bercik@gmail.com>
2024-10-01 14:38:43 +0200

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { ModeToggle } from "./ui/mode-toggle";
-
 function App() {
-  return <ModeToggle />;
+  return (
+    <>
+      <div className="grid grid-cols-1 grid-rows-[5em,1fr] lg:grid-cols-[200px,1fr]">
+        <div className="row-span-1 col-span-2">NavBar</div>
+
+        <div className="hidden row-span-1 col-span-1 lg:inline">Aside</div>
+
+        <div className="col-span-1 row-span-1 mx-auto p-4 flex flex-col gap-4">
+          Main
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;

b870a6c0a99d1751648cbc05ddc443f8c9d2a402
Ondřej Berčík <ondrej.bercik@gmail.com>
2024-10-03 12:26:48 +0200

    Customize games grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const keys = Array.from({ length: 16 }, (_, num) => num + 1);
   return (
-    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {error && <p className="text-red-500 text-4xl">{error}</p>}
 
       {isLoading && keys.map((skeleton) => <GameCardSkeleton key={skeleton} />)}

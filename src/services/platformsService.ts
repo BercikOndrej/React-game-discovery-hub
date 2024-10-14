0196106fee52c2532820e2e80e6dc5c69c7fc55e
Ondřej Berčík <ondrej.bercik@gmail.com>
2024-10-14 20:50:44 +0200

    Refactoring -> deleting service classes

--- src/services/platformsService.ts
+++
@@ -1,6 +0,0 @@
-import { Platform } from '@/entities/Platform';
-import APIClient from './APIClient';
-
-const PLATFORMS_ENDPOINT = '/platforms/lists/parents';
-
-export default new APIClient<Platform>(PLATFORMS_ENDPOINT);
